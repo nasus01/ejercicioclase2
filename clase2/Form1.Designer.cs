61b91208e2fc688650aad1131152a8643d87ff53
cristian ermes morales loaiza <christianjack01@hotmail.com>
2019-09-01 13:36:42 -0500

    codigo donde se implementa  el uso de picturebox

---
+++ clase2/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace clase2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptbimagen5 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen2 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen3 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen4 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen1 = new System.Windows.Forms.PictureBox();
+            this.btnocultar = new System.Windows.Forms.Button();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.txtimagen = new System.Windows.Forms.TextBox();
+            this.btntodos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbimagen5
+            // 
+            this.ptbimagen5.Image = global::clase2.Properties.Resources.PinClipart_com_be_kind_clipart_3827096;
+            this.ptbimagen5.Location = new System.Drawing.Point(595, 86);
+            this.ptbimagen5.Name = "ptbimagen5";
+            this.ptbimagen5.Size = new System.Drawing.Size(100, 50);
+            this.ptbimagen5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen5.TabIndex = 4;
+            this.ptbimagen5.TabStop = false;
+            this.ptbimagen5.DoubleClick += new System.EventHandler(this.ptbimagen5_DoubleClick);
+            // 
+            // ptbimagen2
+            // 
+            this.ptbimagen2.Image = global::clase2.Properties.Resources.kisspng_grupo_sura_insurance_agent_seguro_obligatorio_de_a_sura_5b24d77df00a23_7009866415291411179832;
+            this.ptbimagen2.Location = new System.Drawing.Point(214, 86);
+            this.ptbimagen2.Name = "ptbimagen2";
+            this.ptbimagen2.Size = new System.Drawing.Size(100, 50);
+            this.ptbimagen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen2.TabIndex = 3;
+            this.ptbimagen2.TabStop = false;
+            this.ptbimagen2.DoubleClick += new System.EventHandler(this.ptbimagen2_DoubleClick);
+            // 
+            // ptbimagen3
+            // 
+            this.ptbimagen3.Image = global::clase2.Properties.Resources.logo_1024x800;
+            this.ptbimagen3.Location = new System.Drawing.Point(331, 86);
+            this.ptbimagen3.Name = "ptbimagen3";
+            this.ptbimagen3.Size = new System.Drawing.Size(100, 50);
+            this.ptbimagen3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen3.TabIndex = 2;
+            this.ptbimagen3.TabStop = false;
+            this.ptbimagen3.DoubleClick += new System.EventHandler(this.ptbimagen3_DoubleClick);
+            // 
+            // ptbimagen4
+            // 
+            this.ptbimagen4.Image = global::clase2.Properties.Resources.postobon_logo_31569E7991_seeklogo_com_;
+            this.ptbimagen4.Location = new System.Drawing.Point(462, 86);
+            this.ptbimagen4.Name = "ptbimagen4";
+            this.ptbimagen4.Size = new System.Drawing.Size(100, 50);
+            this.ptbimagen4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen4.TabIndex = 1;
+            this.ptbimagen4.TabStop = false;
+            this.ptbimagen4.DoubleClick += new System.EventHandler(this.ptbimagen4_DoubleClick);
+            // 
+            // ptbimagen1
+            // 
+            this.ptbimagen1.Image = global::clase2.Properties.Resources.Banco_de_Bogotá;
+            this.ptbimagen1.Location = new System.Drawing.Point(83, 86);
+            this.ptbimagen1.Name = "ptbimagen1";
+            this.ptbimagen1.Size = new System.Drawing.Size(100, 50);
+            this.ptbimagen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen1.TabIndex = 0;
+            this.ptbimagen1.TabStop = false;
+            this.ptbimagen1.Click += new System.EventHandler(this.ptbimagen1_Click);
+            this.ptbimagen1.DoubleClick += new System.EventHandler(this.ptbimagen1_DoubleClick);
+            // 
+            // btnocultar
+            // 
+            this.btnocultar.Location = new System.Drawing.Point(107, 281);
+            this.btnocultar.Name = "btnocultar";
+            this.btnocultar.Size = new System.Drawing.Size(75, 23);
+            this.btnocultar.TabIndex = 5;
+            this.btnocultar.Text = "ocultar";
+            this.btnocultar.UseVisualStyleBackColor = true;
+            this.btnocultar.Click += new System.EventHandler(this.btnocultar_Click);
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Location = new System.Drawing.Point(108, 331);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnmostrar.TabIndex = 6;
+            this.btnmostrar.Text = "mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // txtimagen
+            // 
+            this.txtimagen.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtimagen.Location = new System.Drawing.Point(214, 310);
+            this.txtimagen.Name = "txtimagen";
+            this.txtimagen.Size = new System.Drawing.Size(100, 80);
+            this.txtimagen.TabIndex = 0;
+            this.txtimagen.TextChanged += new System.EventHandler(this.txtimagen_TextChanged);
+            // 
+            // btntodos
+            // 
+            this.btntodos.Location = new System.Drawing.Point(108, 386);
+            this.btntodos.Name = "btntodos";
+            this.btntodos.Size = new System.Drawing.Size(75, 23);
+            this.btntodos.TabIndex = 7;
+            this.btntodos.Text = "mostrar todos";
+            this.btntodos.UseVisualStyleBackColor = true;
+            this.btntodos.Click += new System.EventHandler(this.btntodos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btntodos);
+            this.Controls.Add(this.txtimagen);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.btnocultar);
+            this.Controls.Add(this.ptbimagen5);
+            this.Controls.Add(this.ptbimagen2);
+            this.Controls.Add(this.ptbimagen3);
+            this.Controls.Add(this.ptbimagen4);
+            this.Controls.Add(this.ptbimagen1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbimagen1;
+        private System.Windows.Forms.PictureBox ptbimagen4;
+        private System.Windows.Forms.PictureBox ptbimagen3;
+        private System.Windows.Forms.PictureBox ptbimagen2;
+        private System.Windows.Forms.PictureBox ptbimagen5;
+        private System.Windows.Forms.Button btnocultar;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.TextBox txtimagen;
+        private System.Windows.Forms.Button btntodos;
+    }
+}
+
